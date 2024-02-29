a69be3f2f421c533659dd3286d7052417e8b6677
MUKILTEO\1620628 <1620628@mukilteo.wednet.edu>
2024-02-29 12:22:53 -0800

    added button script, added catfish animation

--- Assets/Jonas/Button.cs
+++ Assets/Jonas/Button.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Button : MonoBehaviour
 {
+    public UnityEvent onButtonPressed;
+    public UnityEvent onButtonUnpressed;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //void Start()
+    //{
 
-    // Update is called once per frame
-    void Update()
+    //}
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.CompareTag("Player") && onButtonPressed != null) onButtonPressed.Invoke();
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && onButtonUnpressed != null) onButtonUnpressed.Invoke();
     }
 }
